6c67570367f2803ccd1f4af4b62b0090de042691
rogermedico <roger.medico@gmail.com>
2021-11-16 19:54:37 +0000

    home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,34 @@
 <div>
 </div>
 <div>
-    <h1 class="serif">{{__('home')}}</h1>
-    <p>{{ __('still thinking about that page :)') }}</p>
+    <h1 class="home-h1">{{__('You need a complex web aplication? Maybe some special features that common CMS can\'t fulfill? A huge DB to efficiently manage? A custom API?')}}</h1>
+    <p class="home-h2">{{__('If the response to at last one of that questions is yes, contact me. I can transform your thoughts into a modern and secure web app.')}}</p>
+    <div class="d-flex align-items-center justify-content-around mb-5">
+        <div class="col-2">
+        <img class="img-fluid" src="{{asset('storage/logos/angular.png')}}" alt="Angular">
+        </div>
+        <div class="col-2">
+        <img class="img-fluid" src="{{asset('storage/logos/angular-material.png')}}" alt="Angular Material">
+        </div>
+        <div class="col-2">
+        <img class="img-fluid" src="{{asset('storage/logos/bootstrap.png')}}" alt="Bootstrap">
+        </div>
+        <div class="col-2">
+        <img class="img-fluid" src="{{asset('storage/logos/tailwind.png')}}" alt="Tailwind">
+        </div>
+    </div>
+    <div class="d-flex align-items-center justify-content-around mb-5">
+        <div class="col-1"></div>
+        <div class="col-2">
+            <img class="img-fluid" src="{{asset('storage/logos/laravel.png')}}" alt="Laravel">
+        </div>
+        <div class="col-2">
+            <img class="img-fluid" src="{{asset('storage/logos/php8.png')}}" alt="PHP">
+        </div>
+        <div class="col-2">
+            <img class="img-fluid" src="{{asset('storage/logos/mysql.png')}}" alt="MySQL">
+        </div>
+        <div class="col-1"></div>
+    </div>
+    <p class="small muted text-center">{{__('If you need a comon and boring wordpress page I can do it aswell, so... CONTACT CONTACT!')}}</p>
 </div>
