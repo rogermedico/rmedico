600008d9787004e11488e23ac1bf7990d8717d64
rogermedico <roger.medico@gmail.com>
2021-11-26 10:17:05 +0000

    short title in home low resolutions

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <h1 class="home-h1">{{__('You need a complex web application? Maybe some special features that common CMS can\'t fulfill? A huge DB to efficiently manage? A custom API?')}}</h1>
+    <h1 class="home-h1">{{__('You need a complex web application? ')}}<span class="d-none d-md-inline">{{__('Maybe some special features that common CMS can\'t fulfill? ')}}</span>{{__('A huge DB to efficiently manage? A custom API?')}}</h1>
     <p class="home-h2">{{__('If the response to at last one of that questions is yes, contact me. I can transform your thoughts into a modern and secure web app.')}}</p>
     <div class="d-flex align-items-center justify-content-around mb-3 mb-sm-5">
         <div class="col-md-2">
